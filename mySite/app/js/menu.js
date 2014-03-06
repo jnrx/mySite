bc4f0e56dcbfafb17f6fea5a984e0e2274e71103
jnrx <leiwfong@gmail.com>
2014-03-06 15:51:50 -0800

    Merge origin/experimental1 into experimental1
    Conflicts:
    	mySite/app/css/main.css
    	mySite/app/css/main.scss

--- mySite/app/js/menu.js
+++ mySite/app/js/menu.js
@@ -10,22 +10,36 @@ jQuery.noConflict();
 (function($) {
     $.fn.mainMenu = function() {
         var myID = "#" + $(this).attr("id");
-        $(myID).showMenu();
+        $(window).on("resize load", function() {
+//console.log($("body").find(".navbar-toggle").css("display"));
+            if ($("body").find(".navbar-toggle").css("display") === "none") {
+//console.log("myID="+myID);
+                $("body").off();
+                $(myID).hideMenu();
 
-        $("body").on('click', function(event) {
-            event.stopPropagation();
-            if ($(this).find(myID).hasClass("nav-open")) {
-                $(this).find(myID).hideMenu();
-            }
-        });
+                $("body").on('click', function(event) {
+//console.log("Body Clicked");
+                    event.stopPropagation();
+                    if ($(this).find(myID).hasClass("nav-open")) {
+                        $(this).find(myID).hideMenu();
+                    }
+                });
 
-        $("body").on('click', myID, function(event) {
-            event.stopPropagation();
-            if ($(this).hasClass("nav-open")) {
-                $(this).hideMenu();
-            } else {
-                $(this).showMenu();
-            }
+                $("body").on('click', myID, function(event) {
+//console.log("Menu Clicked");
+                    event.stopPropagation();
+                    if ($(this).hasClass("nav-open")) {
+                        $(this).hideMenu();
+                    } else {
+//console.log("Menu Closed");
+                        $(this).showMenu();
+                    }
+                });
+//            console.log("this is none");
+           } else {
+               $("body").off();
+
+           }
         });
     };
     $.fn.hideMenu = function() {
