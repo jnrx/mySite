12da3bdf853a3af93f29521577ddf05df891f370
jnrx <leiwfong@gmail.com>
2014-03-13 16:46:25 -0700

    Merge origin/experimental1 into experimental1
    Conflicts:
    	mySite/app/css/main.css
    	mySite/app/css/main.scss

--- mySite/app/js/menu.js
+++ mySite/app/js/menu.js
@@ -8,47 +8,53 @@
 jQuery.noConflict();
 
 (function($) {
-    $.fn.mainMenu = function(opt) {
+    $.fn.mainMenu = function() {
         var $this = $(this);
-        $(window).on("resize load", function() {
-//console.log($("body").find(".navbar-toggle").css("display"));
+/*
+        var chkNavTab = function() {
             if ($this.find(".navbar-tab").css("display") === "block") {
-//console.log("myID="+myID);
-                $this.off();
-                $this.hideMenu();
+                return true;
+            } else {
+                return false;
+            }
+        };
+*/        
+        if ($this.find(".navbar-tab").css("display") === "block") {
+            $this.off();
+            $this.hideMenu();
 
-                $("body").find(".mainCanvas").on('click', function(event) {
+            $("body").find(".mainCanvas").on('click', function(event) {
 //console.log("Canvas Clicked");
-                    if ($this.hasClass("nav-open")) {
-                        $this.hideMenu();
+                if ($this.hasClass("nav-open")) {
+                    $this.hideMenu();
 //                        event.stopPropagation();
-                    }
-                });
+                }
+            });
 //console.log($this.find('.navbar-tab').attr('class'));
-                $this.find(".navbar-tab").on('click', function() {
+            $this.find(".navbar-tab").on('click', function() {
 //console.log("Menu Clicked");
-                    if ($this.hasClass("nav-open")) {
-                        $this.hideMenu();
+                if ($this.hasClass("nav-open")) {
+                    $this.hideMenu();
 //                        event.stopPropagation();
-                    } else {
+                } else {
 //console.log("Menu Closed");
-                        $this.showMenu();
+                    $this.showMenu();
 //                        event.stopPropagation();
-                    }
-                });
-           } else {
+                }
+            });
 //console.log("this is none");
 //               $("body").off();
 
-           }
-        });
+        }
     };
     $.fn.hideMenu = function() {
+        $(this).find("img").attr("src", "img/al2r.gif");
         $(this).removeClass("nav-open");
         $(this).addClass("nav-close");
     };
 
     $.fn.showMenu = function() {
+        $(this).find("img").attr("src", "img/ar2l.gif");
         $(this).removeClass("nav-close");
         $(this).addClass("nav-open");
    };
